d6f54ddc9c94a3d5beac4ee87cbf91a33e91d73a
Clairejing <wenjing.liu09@foxmail.com>
2016-03-13 23:51:30 +0800

    Complete the initial userInfo page

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/cart', function(req, res, next){
   res.render('cart',{
     title: 'Cart',
     totalPrice:200,
+    user:{name:"Claire"},
     dishArray:[
       {dishName:"Tudousi",unitPrice:"20",quantity:1},
       {dishName:"Paigu",unitPrice:"40",quantity:2},
@@ -21,7 +22,8 @@ router.get('/cart', function(req, res, next){
 
 /*GET userInfo page*/
 router.get('/userInfo', function(req, res, next){
-  res.render('userInfo',{title: 'Me'});
+  res.render('userInfo',{title: "Me", user:{image:"http://img4.imgtn.bdimg.com/it/u=432839616,2165665298&fm=21&gp=0.jpg", name:"Claire"}});
+  // res.render('userInfo',{title: "Me"});
 });
 
 module.exports = router;
