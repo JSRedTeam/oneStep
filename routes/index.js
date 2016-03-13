7f48a3523d4bab2a907fa7064e3223dc209e1f3f
Clairejing <wenjing.liu09@foxmail.com>
2016-03-13 18:43:25 +0800

    Complete the initial cart.handlebars page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Claire' });
+  res.render('index', { title: 'Claire'});
+});
+
+/*GET Cart page.*/
+router.get('/cart', function(req, res, next){
+  res.render('cart',{
+    title: 'Cart',
+    totalPrice:200,
+    dishArray:[
+      {dishName:"Tudousi",unitPrice:"20",quantity:1},
+      {dishName:"Paigu",unitPrice:"40",quantity:2},
+      {dishName:"Guobaorou",unitPrice:"50",quantity:1}
+    ]
+    });
+});
+
+/*GET userInfo page*/
+router.get('/userInfo', function(req, res, next){
+  res.render('userInfo',{title: 'Me'});
 });
 
 module.exports = router;
