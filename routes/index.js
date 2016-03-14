44d7c271b50a7dfe5984797c6aa98b5fad37e9f1
Clairejing <wenjing.liu09@foxmail.com>
2016-03-14 10:03:25 +0800

    Change the font-size of userInfo.css

--- routes/index.js
+++ routes/index.js
@@ -22,8 +22,8 @@ router.get('/cart', function(req, res, next){
 
 /*GET userInfo page*/
 router.get('/userInfo', function(req, res, next){
-  res.render('userInfo',{title: "Me", user:{image:"http://img4.imgtn.bdimg.com/it/u=432839616,2165665298&fm=21&gp=0.jpg", name:"Claire"}});
-  // res.render('userInfo',{title: "Me"});
+  // res.render('userInfo',{title: "Me", user:{image:"http://img4.imgtn.bdimg.com/it/u=432839616,2165665298&fm=21&gp=0.jpg", name:"Claire"}});
+  res.render('userInfo',{title: "Me"});
 });
 
 module.exports = router;
